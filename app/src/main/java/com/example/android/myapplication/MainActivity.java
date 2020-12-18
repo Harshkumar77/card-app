3f173ee6e2c452784be42df52cdb7188b7667326
Your Name <you@example.com>
2020-12-19 01:28:02 +0530

    Added all the cards in raw folder

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -8,12 +8,13 @@ import androidx.viewpager2.widget.ViewPager2;
 public class MainActivity extends AppCompatActivity {
 
     ViewPager2 viewPager2;
-    boolean orientationHorizontal = true ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(getSupportActionBar()!=null)
+            getSupportActionBar().setTitle("Card App");
         viewPager2 = findViewById (R.id.viewPager2);
         viewPager2.setAdapter(new viewPagerAdapter(this));
         viewPager2.setOrientation(ViewPager2.ORIENTATION_VERTICAL);

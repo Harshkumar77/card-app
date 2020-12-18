adc425f175ed0c6ba093447403683ccf3a1dd5e0
Your Name <you@example.com>
2020-12-18 16:58:01 +0530

    added dependencies for ViewPager2

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.android.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager2 viewPager2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

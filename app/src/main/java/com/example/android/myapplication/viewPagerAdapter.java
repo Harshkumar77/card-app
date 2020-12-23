3764a1d9443ba1a7a5948b812c9ddcbdd03253f2
Your Name <you@example.com>
2020-12-23 16:44:09 +0530

    Documented the app

--- app/src/main/java/com/example/android/myapplication/viewPagerAdapter.java
+++ app/src/main/java/com/example/android/myapplication/viewPagerAdapter.java
@@ -19,10 +19,24 @@ import java.util.ArrayList;
 
 public class viewPagerAdapter extends RecyclerView.Adapter<viewPagerAdapter.ViewHolder> {
 
+    /**
+     * viewPager2Adapter is a custom Adapter to hold ViewHolder
+     * This Adapter override methods from RecycleView.Adapter<>
+     */
+
     private Context context;
     private ArrayList<cardStructure> cardStructureArrayList;
     private TabLayout tabLayout;
     private ViewPager2 viewPager2;
+
+    /**
+     * constructor for viewPagerAdapter
+     *
+     * @param context                context of main activity
+     * @param cardStructureArrayList ArrayList containing all the info about cards
+     * @param tabLayout              TabLayout in which tab will be added
+     * @param viewPager2             ViewPager2 in which cards will be added
+     */
 
     public viewPagerAdapter(Context context, ArrayList<cardStructure> cardStructureArrayList, TabLayout tabLayout, ViewPager2 viewPager2) {
         this.context = context;
@@ -31,35 +45,67 @@ public class viewPagerAdapter extends RecyclerView.Adapter<viewPagerAdapter.View
         this.viewPager2 = viewPager2;
     }
 
+    /**
+     * When ViewHolder is created
+     *
+     * @param parent   ViewGroup which will be displayed when ViewHolder is created
+     * @param viewType .
+     * @return ViewHolder which would be displayed
+     */
+
     @NonNull
     @Override
-    public viewPagerAdapter. ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public viewPagerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        /* Attaching TabLayoutMediator with the viewPager2 for creating swipe views */
         new TabLayoutMediator(tabLayout, viewPager2,
                 (tab, position) -> tab.setText(cardStructureArrayList.get(position).cardName)
+                // A lambda expression to add Tab in TabLayout
         ).attach();
+        // Inflating Layout file to pass in view holder
         return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.cell_item, parent, false));
     }
 
+    /**
+     * When ViewPager2 is holding ViewHolder
+     *
+     * @param holder   ViewHolder which is being on hold
+     * @param position position of current ViewHolder
+     */
+
     @Override
     public void onBindViewHolder(@NonNull viewPagerAdapter.ViewHolder holder, int position) {
+        // Modifying the ViewHolder according to the card info
         holder.mText.setText(cardStructureArrayList.get(position).cardName);
-        if(cardStructureArrayList.get(position).isBlack)
-            holder.mText.setTextColor(ContextCompat.getColor(context,android.R.color.black));
+        if (cardStructureArrayList.get(position).isBlack)
+            holder.mText.setTextColor(ContextCompat.getColor(context, android.R.color.black));
         else
-            holder.mText.setTextColor(ContextCompat.getColor(context,android.R.color.holo_red_dark));
+            holder.mText.setTextColor(ContextCompat.getColor(context, android.R.color.holo_red_dark));
         holder.mImage.setImageResource(cardStructureArrayList.get(position).cardImageId);
     }
+
+    /**
+     * @return total number of ViewHolder
+     */
 
     @Override
     public int getItemCount() {
         return cardStructureArrayList.size();
     }
 
+    /**
+     * Custom View that is used by viewPagerAdapter
+     **/
+
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
         public TextView mText;
         public ImageView mImage;
 
+        /**
+         * Constructor to construct ViewHolder
+         *
+         * @param itemView Inflated Layout file into view
+         */
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             mText = itemView.findViewById(R.id.text);

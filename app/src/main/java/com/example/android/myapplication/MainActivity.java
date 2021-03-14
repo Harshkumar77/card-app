d9f86b9aa70e7cddef8e62309e343155aef5417f
Your Name <you@example.com>
2021-03-15 01:32:45 +0530

    f

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.android.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager2.widget.ViewPager2;
 
 import java.util.ArrayList;
@@ -11,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<cardStructure> cardStructureArrayList = new ArrayList<>();
     // ArrayList of all the cards
+    TextView hl, vl;
+    boolean isHorizontal = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,24 @@ public class MainActivity extends AppCompatActivity {
         viewPager2.setAdapter(new viewPagerAdapter(this, cardStructureArrayList, findViewById(R.id.tabs), viewPager2));
         //setting viewPagerAdapter with viewPager
 
-        viewPager2.setOrientation(ViewPager2.ORIENTATION_VERTICAL); // setting orientation of viewPager
+        hl = findViewById(R.id.hl);
+        vl = findViewById(R.id.vl);
+
+        hl.setOnClickListener(v -> {
+            if (!isHorizontal) {
+                isHorizontal = true;
+                viewPager2.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+                hl.setBackgroundColor(ContextCompat.getColor(getBaseContext() , android.R.color.darker_gray));
+                vl.setBackgroundColor(ContextCompat.getColor(getBaseContext() , android.R.color.white));
+            }
+        });
+        vl.setOnClickListener(v -> {
+            if (isHorizontal) {
+                isHorizontal = false;
+                viewPager2.setOrientation(ViewPager2.ORIENTATION_VERTICAL);
+                vl.setBackgroundColor(ContextCompat.getColor(getBaseContext() , android.R.color.darker_gray));
+                hl.setBackgroundColor(ContextCompat.getColor(getBaseContext() , android.R.color.white));
+            }
+        });
     }
 }
